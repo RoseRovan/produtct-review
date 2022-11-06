9bf6f9317fffdfd413a0787b871a6d86bf3d7a42
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-11-06 20:26:27 +0300

    ft-ratings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
     has_many :reviews
-    has_many :products, through: :reviews  
+    has_many :products, through: :reviews 
+     
     def favorite_product
-
+        self.reviews.order(:star_ratings).last
     end
 
     def remove_reviews(product)
